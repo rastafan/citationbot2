de3a4fc9e5cacd3dee3b100415c19b9c1dc87306
Matteo Gironi <rastafanimplacheibol@libero.it>
2016-08-01 19:08:04 +0200

    -commentata la risposta di default

--- www/bot_work.php
+++ www/bot_work.php
@@ -171,9 +171,9 @@ function processMessage($message) {
       // stop now
 	  //gif\faizon_charge.gif
 	  apiRequestDocument($chat_id, realpath("gif/faizon_charge.gif"),$message_id);
-    } else {
+    } /*else {
       apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => 'Cool'));
-    }
+    }*/
   } else {
     //apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
   }
